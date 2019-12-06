4bd3ac9ed159b87d04552e3a71884781a2763874
wanzhs_cedar <wanzhs_cedar@163.com>
2019-12-06 09:35:10 +0800

    spring security

--- eureka-client/src/main/java/com/ga/com/eureka/client/ClientApplication.java
+++ eureka-client/src/main/java/com/ga/com/eureka/client/ClientApplication.java
@@ -2,13 +2,11 @@ package com.ga.com.eureka.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+//import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableEurekaClient
-@EnableFeignClients(basePackages = "com.ga.com.eureka.client.fein")
+//@EnableEurekaClient
+//@EnableFeignClients(basePackages = "com.ga.com.eureka.client.fein")
 public class ClientApplication {
 
     public static void main(String[] args) {
--- eureka-client/src/main/java/com/ga/com/eureka/client/controller/TestController.java
+++ eureka-client/src/main/java/com/ga/com/eureka/client/controller/TestController.java
@@ -1,23 +1,24 @@
 package com.ga.com.eureka.client.controller;
 
-import com.ga.com.eureka.client.fein.TestService;
+//import com.ga.com.eureka.client.fein.TestService;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
-
 @RestController
 @RequestMapping("/test")
 public class TestController {
-    @Autowired
-    TestService testService;
+//    @Autowired
+//    TestService testService;
 
     @GetMapping("/parameter")
     public String getParameter(@RequestParam(value = "parameter", required = true) String parameter) {
-        String rs = testService.echo(parameter);
-        return rs;
+//        String rs = testService.echo(parameter);
+//        return rs;
+        return null;
     }
 }
