149b3cba0aaa42ab4b6c513b31b343eed9a3d790
wanzhs_cedar <wanzhs_cedar@163.com>
2019-12-23 14:44:16 +0800

    运行情况监控+security

--- eureka-server/src/main/java/com/ga/com/eureka/server/config/WebSecurityConfig.java
+++ eureka-server/src/main/java/com/ga/com/eureka/server/config/WebSecurityConfig.java
@@ -9,26 +9,16 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-//public class WebSecurityConfig{
-    /**
-     * 高版本的丢弃了
-     *
-     * security:
-     *   basic:
-     *    enabled: true
-     *
-     * 配置，应该使用以下方式开启
-     *
-     * @param http
-     * @throws Exception
-     */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // Configure HttpSecurity as needed (e.g. enable http basic).
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER);
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED);
         http.csrf().disable();
         //注意：为了可以使用 http://${user}:${password}@${host}:${port}/eureka/ 这种方式登录,所以必须是httpBasic,
         // 如果是form方式,不能使用url格式登录
-        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+        http.authorizeRequests()
+                .antMatchers("/actuator/*").permitAll()
+                .antMatchers("/actuator/metrics/*").permitAll()
+                .anyRequest().authenticated().and().httpBasic();
     }
 }
